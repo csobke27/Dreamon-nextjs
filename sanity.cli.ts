6b45a4dbf604f4378576ef8550b42a39db9bdd6b
csobke27 <csobke@gmail.com>
2026-08-28 17:35:15 -0400

    Update .gitignore, package.json, and sanity configuration; add dotenv support and remove unused studio page

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,8 +3,18 @@
 * Go to https://www.sanity.io/docs/cli to learn more.
 **/
 import { defineCliConfig } from 'sanity/cli'
+import { config } from 'dotenv'
+
+config({ path: '.env.local' })
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig(
+    {
+        api: { projectId, dataset },
+        deployment: {
+            appId: 'n0v48qkgmcuo0tdlngraiccp',
+        },
+    }
+)
